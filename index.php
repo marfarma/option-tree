1d93f716393069cd1ffda80dd10b838408d57153
valendesigns <valendesigns@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-03 06:09:58 +0000

    updating version text
    git-svn-id: http://svn.wp-plugins.org/option-tree/tags/1.1@307215 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: OptionTree
 Plugin URI: http://wp.envato.com
 Description: Customizable WordPress Theme Options Framework
-Version: 1.0.0
+Version: 1.1
 Author: Derek Herman
 Author URI: http://valendesigns.com
 */
