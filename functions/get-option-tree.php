fc5915aec6306a733df500bf2a0ef34080f1ddf0
valendesigns <valendesigns@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-02 13:42:56 +0000

    tagging version 1.1.2
    git-svn-id: http://svn.wp-plugins.org/option-tree/tags/1.1.2@339899 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions/get-option-tree.php
@@ -0,0 +1,46 @@
+<?php if (!defined('OT_VERSION')) exit('No direct script access allowed');
+/**
+ * Displays or returns a value from the 'option_tree' array.
+ *
+ * @uses get_option()
+ *
+ * @access public
+ * @since 1.0.0
+ *
+ * @param string $item_id
+ * @param array $options
+ * @param bool $echo
+ * @param bool $is_array
+ * @param int $offset
+ *
+ * @return mixed
+ */
+function get_option_tree( $item_id = '', $options = '', $echo = false, $is_array = false, $offset = -1) 
+{  
+  // load saved options
+  if ( !$options )
+    $options = get_option( 'option_tree' );
+  
+  // set the item
+  if ( !isset( $options[$item_id] ) )
+    return;
+  
+  // single item value  
+  $content = stripslashes($options[$item_id]);
+  
+  // create an array of values
+  if ( $is_array ) 
+  {
+    $content = explode( ',', $content );
+    if ( is_numeric( $offset ) && $offset >= 0) 
+    {
+      $content = htmlspecialchars_decode( trim( $content[$offset] ) );
+    }
+  }
+  
+  // echo content
+  if ($echo)
+    echo $content;
+  
+  return $content;
+}
